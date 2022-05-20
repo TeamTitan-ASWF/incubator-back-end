e613c178adbc097ad203eca21f8722c3052e44d1
Jose R. Estrella Sr <josephstar48@hotmail.com>
2022-05-20 12:31:21 -0500

    2_S_Story1_004 added controller, service, error handling, custom not found error, testing. Does not patch correctly yet"

---
+++ src/main/java/com/incubator/ApplicationErrorHandler.java
@@ -0,0 +1,14 @@
+package com.incubator;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApplicationErrorHandler {
+    @ExceptionHandler(IncubatorNotFound.class)
+    public ResponseEntity<String> handleIncubatorNotFound(IncubatorNotFound e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
