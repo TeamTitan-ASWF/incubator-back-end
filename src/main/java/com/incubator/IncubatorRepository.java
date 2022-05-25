0a18691a6983f9feeff51642627c0e3fb8342092
danielgebhardt <daniel.h.gebhardt.mil@swf.army.mil>
2022-05-25 13:19:10 -0500

    added custom query for IncubatorList to be used for Listing Applications

--- src/main/java/com/incubator/IncubatorRepository.java
+++ src/main/java/com/incubator/IncubatorRepository.java
@@ -1,18 +1,20 @@
 package com.incubator;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.text.html.Option;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
-public interface IncubatorRepository extends CrudRepository <Incubator, Long> {
+public interface IncubatorRepository extends JpaRepository<Incubator, Long> {
 
-//    @Query("select u.id, u.fName, u.mI, u.lName, u.rank, u.dateSubmitted, u.status from Incubator u")
-//    //@Query(value = "select id, f_Name, mI, l_Name, rank, date_Submitted, status from incubators", nativeQuery = true)
-//    public Iterable<IncubatorList> getReviewerApplicationList();
+    @Query("select new com.incubator.IncubatorList(u.id, u.fName, u.lName, u.mI, u.rank, u.status, u.dateSubmitted) from Incubator AS u order by u.lName")
+    //@Query(value = "select id, f_Name, mI, l_Name, rank, date_Submitted, status from incubators", nativeQuery = true)
+    public List<IncubatorList> getReviewerApplicationList();
 
     public Optional<Incubator> findFirstByDodIdEquals(String dodId);
 }
